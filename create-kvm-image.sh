857327520db5c238e9a855a599146df86e5dd496
Pierre Zemb <pierre.zemb.isen@gmail.com>
2016-03-13 13:31:27 +0100

    Update create-kvm-image.sh

--- create-kvm-image.sh
+++ create-kvm-image.sh
@@ -16,10 +16,6 @@
 # Heavily inspired from:
 # http://git.exherbo.org/exherbo.git/tree/scripts/create-kvm-image
 
-
-logfile=/var/log/exherbo.log
-exec > $logfile 2>&1
-
 # print usage instructions
 usage() {
     cat <<EOF
